23ab58546dc1c95942734b79039a9d0d8965a522
Pierce <pierce@vulpes.dev>
2021-09-12 15:18:50 -0400

    update silenced pistol with example of hitgroup damage multipliers

--- lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
+++ lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
@@ -2,5 +2,14 @@ AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "weapon_ttt_sipistol" )
-    weapon.Primary.Damage = 10000000000
+    weapon.Primary.Damage = 100
+    weapon.CFCDamageMultipliers = {
+        [HITGROUP_HEAD] = 2, -- 2X normal head damage
+        [HITGROUP_CHEST] = 0.2,
+        [HITGROUP_STOMACH] = 0.1,
+        [HITGROUP_LEFTARM] = 0.1,
+        [HITGROUP_RIGHTARM] = 0.1,
+        [HITGROUP_LEFTLEG] = 0.1,
+        [HITGROUP_RIGHTLEG] = 0.1
+    }
 end )
