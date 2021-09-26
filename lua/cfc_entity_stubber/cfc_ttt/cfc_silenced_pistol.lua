74a729a0a7f1df54e90a51a3135d1ff1abc03bab
plally <pierce@vulpes.dev>
2021-09-25 20:04:19 -0400

    Update cfc_silenced_pistol.lua

--- lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
+++ lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
@@ -1,18 +1,4 @@
 AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
-    local weapon = cfcEntityStubber.getWeapon( "weapon_ttt_sipistol" )
-    weapon.Primary.Damage = 100
-    weapon.CFCDamageMultipliers = {
-        [HITGROUP_HEAD] = 2, -- 2X normal head damage
-        [HITGROUP_CHEST] = 0.2,
-        [HITGROUP_STOMACH] = 0.1,
-        [HITGROUP_LEFTARM] = 0.1,
-        [HITGROUP_RIGHTARM] = 0.1,
-        [HITGROUP_LEFTLEG] = 0.1,
-        [HITGROUP_RIGHTLEG] = 0.1
-    }
-    weapon.CFCHitGroupDamageValues = {
-        [HITGROUP_LEFTLEG] = 69 -- always do 69 damage to the left leg
-    }
 end )
