96d2a2929c39820e536caed56025ac5cb3aca403
Pierce <pierce@vulpes.dev>
2022-07-27 23:04:56 -0400

    adjust mp5 status further

--- lua/cfc_entity_stubber/cfc_ttt/weapon_ttt_mp5.lua
+++ lua/cfc_entity_stubber/cfc_ttt/weapon_ttt_mp5.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile()
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "weapon_ttt_mp5" )
-    weapon.Primary.Cone = 0.023
-    weapon.Primary.Damage = 12
+    weapon.Primary.Cone = 0.025
+    weapon.Primary.Damage = 13
 end )
