004d6d34a7087e93442605a89f18678006921a56
Pierce <pierce@vulpes.dev>
2023-09-21 21:06:48 -0400

    update silenced pistol

--- lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
+++ lua/cfc_entity_stubber/cfc_ttt/cfc_silenced_pistol.lua
@@ -1,6 +1,10 @@
 AddCSLuaFile()
 
-cfcEntityStubber.registerStub( function()
+cfcEntityStubber.registerStub(function()
     local weapon = weapons.GetStored("weapon_ttt_sipistol")
+
     weapon.Primary.Damage = 39
-end )
+    weapon.Primary.Recoil = 3
+    weapon.Primary.DefaultClip = 10
+    weapon.Primary.ClipMax = 10
+end)
