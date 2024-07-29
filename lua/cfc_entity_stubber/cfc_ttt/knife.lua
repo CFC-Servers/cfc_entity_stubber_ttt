68495d7d9e1784b0a561d781e073915b4ba9813a
Pierce <pierce@vulpes.dev>
2024-07-28 21:43:13 -0400

    knife buff

--- lua/cfc_entity_stubber/cfc_ttt/knife.lua
+++ lua/cfc_entity_stubber/cfc_ttt/knife.lua
@@ -1,5 +1,6 @@
 AddCSLuaFile()
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "weapon_ttt_knife" )
-    wepaon.DeploySpeed = 4
+    weapon.DeploySpeed = 4
+    weapon.Primary.Damage = 70
 end )
